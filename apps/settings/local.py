9ac10e034858bdc53436a09f75a96a15a30a567c
jmrcas <cas.jm7>
2020-08-20 19:35:49 +0800

    Development - Add Debug-Toolbar

--- apps/settings/local.py
+++ apps/settings/local.py
@@ -13,24 +13,24 @@ SECRET_KEY = 'yf_xdu7kg@bnnk-4)f23=nx!cx2(=o!&7z^_6#t13l0yezxbe6'
 
 INSTALLED_APPS += [
     'django_extensions',
+    'debug_toolbar',
 ]
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'ecommerce',
-        # 'USER': os.environ.get('DB_USER'),
-        # 'PASSWORD': os.environ.get("DB_PASSWORD"),
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '5432'
-    # }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
