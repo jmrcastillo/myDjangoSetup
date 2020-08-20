f3fc684d751cd37f47064fd8e567dd24fdce5d7e
jmrcas <cas.jm7>
2020-08-20 16:04:01 +0800

    Apps Setup

---
+++ apps/settings/local.py
@@ -0,0 +1,29 @@
+
+
+import os
+from .base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
+
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'ecommerce',
+        # 'USER': os.environ.get('DB_USER'),
+        # 'PASSWORD': os.environ.get("DB_PASSWORD"),
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432'
+    # }
+# }
+
---
+++ apps/settings/production.py
@@ -0,0 +1,37 @@
+from .base import *
+import os
+
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'yf_xdu7kg@bnnk-4)f23=nx!cx2(=o!&7z^_6#t13l0yezxbe6'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
